f9504aab503fe458a15aa85b82ba02c363ecb455
Sébastien Loisel <S.Loisel@hw.ac.uk>
2025-12-28 23:20:19 +0100

    Add Service Worker for offline support

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'knight-trainer-v1';
+const urlsToCache = [
+  '/knight-trainer/',
+  '/knight-trainer/index.html',
+  '/knight-trainer/manifest.json'
+];
+
+// Install - cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate - clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch - serve from cache, fallback to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          // Cache new requests
+          if (response.status === 200) {
+            const responseClone = response.clone();
+            caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, responseClone);
+            });
+          }
+          return response;
+        });
+      })
+  );
+});
